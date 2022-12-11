ff67a3506577ec04b4ad6b03fcda0a9453df9429
Ali <proali1997@gmail.com>
2022-12-11 15:40:53 -0800

    Adding Brand

---
+++ ITI.ElectroDev.Models/Order/OrderDetailsConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.ElectroDev.Models
+{
+    public class OrderDetailsConfig : IEntityTypeConfiguration<OrderDetails>
+    {
+        public void Configure(EntityTypeBuilder<OrderDetails> builder)
+        {
+            builder.ToTable("OrderDetails");
+            builder.HasKey(i => i.Id);
+            builder.Property(i => i.Id).IsRequired().ValueGeneratedOnAdd();
+            builder.Property(i => i.UserId).IsRequired();
+            builder.Property(i => i.TotalPrice).IsRequired();
+            builder.Property(i => i.Type).IsRequired().HasMaxLength(200);
+        }
+    }
+}
