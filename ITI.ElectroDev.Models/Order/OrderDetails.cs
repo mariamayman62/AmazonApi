4fca5afd1d3d49c4799aa8f26c35b82e229a6813
User Name <yousefzakaria27@gmail.com>
2022-12-12 13:46:01 +0200

    Merge Brand on Main

---
+++ ITI.ElectroDev.Models/Order/OrderDetails.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.ElectroDev.Models
+{
+    public class OrderDetails
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public double TotalPrice { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
+        public virtual User User { get; set; }
+        public virtual ICollection<OrderItems> OrderItems { get; set; }
+    }
+}
---
+++ ITI.ElectroDev.Models/Order/OrderItemsConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.ElectroDev.Models
+{
+    public class OrderItemsConfig : IEntityTypeConfiguration<OrderItems>
+    {
+        public void Configure(EntityTypeBuilder<OrderItems> builder)
+        {
+            builder.ToTable("OrderItems");
+            builder.HasKey(i => i.Id);
+            builder.Property(i => i.Id).IsRequired().ValueGeneratedOnAdd();
+            builder.Property(i => i.ProductId).IsRequired();
+            builder.Property(i => i.CreatedAt).IsRequired();
+        }
+    }
+}
