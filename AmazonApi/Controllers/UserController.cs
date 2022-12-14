4af3ef31b4b3fbdf7738f3f870d0c00bbfa2fb22
User Name <yousefzakaria27@gmail.com>
2022-12-14 02:08:19 +0200

    Add Login Api

--- AmazonApi/Controllers/UserController.cs
+++ AmazonApi/Controllers/UserController.cs
@@ -3,11 +3,16 @@ using ITI.ElectroDev.Presentation;
 using ITI.Library.Presentation.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace AmazonApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    //[Route("[controller]")]
+    [Route("api/[controller]/[action]")]
     public class UserController : ControllerBase
     {
         Context c;
@@ -69,6 +74,80 @@ namespace AmazonApi.Controllers
             }
             return myModel;
         }
+        [HttpPost]
+        public async Task<ResultViewModel> SignIn([FromBody] LoginModel model)
+        {
+            ResultViewModel myModel = new ResultViewModel();
+            if (ModelState.IsValid == false)
+            {
+                myModel.Success = false;
+                myModel.Data =
+                    ModelState.Values.SelectMany
+                            (i => i.Errors.Select(x => x.ErrorMessage));
+            }
+            else
+            {
+                var result
+                     = await SignInManager.PasswordSignInAsync
+                        (model.UserName, model.Password, model.RememberMe,
+                             true);
+                var user1 = await UserManager.FindByNameAsync(model.UserName);
+
+                  if (user1 is null || ! await UserManager.CheckPasswordAsync(user1, model.Password))
+                {
+                    myModel.Success = false;
+                    myModel.Message = "Invalid UserName Or Password .";
+                }
+                else if (result.IsNotAllowed == true)
+                {
+                    myModel.Success = false;
+                    myModel.Message = "Invalid UserName Or Password ";
+                }
+                else if (result.IsLockedOut)
+                {
+                    myModel.Success = false;
+                    myModel.Message = "Is Locked Out";
+                }
+               
+
+                else
+                {
+                    var user = await UserManager.FindByNameAsync(model.UserName);
+                    List<Claim> claims = new List<Claim>();
+                    var roles = await UserManager.GetRolesAsync(user);
+                    roles.ToList().ForEach(i =>
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, i));
+                    });
+
+                    JwtSecurityToken token
+                        = new JwtSecurityToken
+                        (
+                            signingCredentials:
+                             new SigningCredentials
+                             (
+                                 new SymmetricSecurityKey(Encoding.ASCII.GetBytes("IOLJYHSDSIoleJHsdsdsas98WeWsdsdQweweHgsgdf_&6#2"))
+                                 ,
+                                 SecurityAlgorithms.HmacSha256
+                             ),
+                            expires: DateTime.Now.AddDays(5),
+                            claims: claims
+                        );
+
+                    string tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+                    myModel.Success = true;
+                    myModel.Message = "Successfulyy Loged In";
+                    myModel.Data = new
+                    {
+                        User = user,
+                        Toekn = tokenValue,
+                        Roles = roles
+                    };
+                }
+            }
+            return myModel;
+        }
+
 
     }
 }
